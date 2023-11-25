1ef3a2d928bf3200e3a9c4ef099f07dedd9b8bbd
HolovchukNatalia <120129831+HolovchukNatalia@users.noreply.github.com>
2023-11-25 21:25:04 +0200

    Add files via upload

---
+++ CreateX/scripts/scripts.js
@@ -0,0 +1,20 @@
+// new Swiper('.swiper-container-offers', {
+
+//     navigation: {
+//         nextEI: '.swiper-button-next',
+//         prevEI: '.swiper-button-prev'
+//     },
+// });
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
+
